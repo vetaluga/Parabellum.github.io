575b7521e9c0922550907a43c134f8125a614bfb
vetaluga <vrtalugavetaluga@gmail.com>
2018-05-29 02:59:57 +0300

    Parallelum landing

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+  $(".main-menu_btn").on('click', function(){
+  	$('.ss').toggleClass('main-menu_active');
+  });
+});
